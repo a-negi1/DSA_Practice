5a05933edbdbfd526c92ad05abbbe1a9bd1649fd
a-negi1 <adheeshnegi16@gmail.com>
2026-04-01 00:23:55 +0530

    lc424.

--- Longest Repeating Character Replacement.js
+++ Longest Repeating Character Replacement.js
@@ -1,37 +1,40 @@
-function find(arr) {
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+let find =(a)=>{
     let maxc = -1;
-    for (let i = 0; i < 256; i++) {
-        maxc = Math.max(maxc, arr[i]);
+    for(let i=0;i<256;i++){
+        maxc = Math.max(a[i],maxc);
     }
     return maxc;
 }
-
-var characterReplacement = function (s, k) {
-    let n = s.length;
+var characterReplacement = function(s, k) {
+    let low = 0 ;
     let f = new Array(256).fill(0);
+    
+    let res = -Infinity
+    
+    for(let high=0;high<s.length;high++){
+        f[s.charCodeAt(high)]++; 
+        let len = high - low +1
+        
+        let freq =find (f)
+        let diff = len - freq 
+        while(diff>k){
+            f[s.charCodeAt(low)] -- ;
+            low++
+             len = high - low +1    
+             freq = find (f)
+             diff = len - freq 
 
-    let low = 0;
-    let res = -Infinity;
-
-    for (let high = 0; high < n; high++) {
-        f[s.charCodeAt(high)]++;
-
-        let maxcnt = find(f);
-        let len = high - low + 1;
-        let diff = len - maxcnt;
-
-        while (diff > k) {
-            f[s.charCodeAt(low)]--;
-            low++;
-
-            maxcnt = find(f);
-            len = high - low + 1;
-            diff = len - maxcnt;
+            
         }
 
-        len = high - low + 1;
-        res = Math.max(res, len);
+         len = high - low +1
+        res = Math.max(res,len);
     }
-
-    return res;
+    return res
+    
 };
