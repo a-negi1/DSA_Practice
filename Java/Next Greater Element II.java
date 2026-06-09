79047d308252b4961924eac174d2b48e3b24afb2
a-negi1 <adheeshnegi16@gmail.com>
2026-06-10 03:30:32 +0530

    LC503

---
+++ Java/Next Greater Element II.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        Stack <Integer> st = new Stack<>();
+        int ans[] = new int[n];
+        Arrays.fill(ans,-1);
+        
+        for(int i=n-2;i>=0;i--){
+            st.push(nums[i]);
+        }
+        
+        for(int i=n-1;i>=0;i--){
+            while(st.size()>0 && st.peek() <= nums[i] ){
+                st.pop();
+
+                
+
+            }
+
+            if(st.size() > 0){
+                    ans[i] = st.peek();
+                }
+                
+                st.push(nums[i]);
+
+        }
+        return ans;
+    }
+}
