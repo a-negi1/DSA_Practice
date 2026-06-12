dabe682b3484540bb5220845c09fec8c6dfa15bd
a-negi1 <adheeshnegi16@gmail.com>
2026-06-12 19:42:39 +0530

    LC387

---
+++ Java/First Unique Character in a String.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map <Character,Integer> h = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            char c = s.charAt(i);
+            h.put(c,h.getOrDefault(c,0)+1);
+
+            
+        }
+
+        for(int i=0;i<s.length();i++){
+            if(h.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+
+    }
+}
