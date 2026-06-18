e8cad8e4d7eab4b2c8f672e5c28054d0a35725ff
a-negi1 <adheeshnegi16@gmail.com>
2026-06-18 20:51:47 +0530

    LC24

--- Java/Swap Nodes in Pairs.java
+++ Java/Swap Nodes in Pairs.java
@@ -1,21 +1,82 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode start = new ListNode(0);
-        start.next = head;
-
-        ListNode curr = start;
-
-        while (curr.next != null && curr.next.next != null) {
-            ListNode node1 = curr.next;
-            ListNode node2 = node1.next;
-
-            node1.next = node2.next;
-            node2.next = node1;
-            curr.next = node2;
-
-            curr = node1;
+        if(head==null ){
+            return null;
         }
+        ListNode res = null;
+        ListNode prevleft = null;
+        ListNode left = head;
+        ListNode right;
+        int size = 2;
 
-        return start.next;
+        while(left!=null){
+            
+                right = left;
+                for(int i=0;i<size-1;i++){
+                    
+                    if(right== null){
+                        break;
+                    }
+                    else{
+                        right = right.next;
+                    }
+                    
+                }
+                
+                if(right!=null){
+                    ListNode nextleft = right.next;
+                    reverse(left,size);
+                    if(prevleft !=null){
+                        prevleft.next = right;
+                    }
+                    if(res==null){
+                        res=right;
+                    }
+                    prevleft = left ;
+                    left = nextleft;
+                }
+                else{
+                    if(prevleft != null ){
+                        prevleft.next = left;
+                    }
+                    if(res==null){
+                        res=left;
+                    }
+                    break;
+                    
+                }
+
+                
+                
+            
+            
+
+            
+        }
+        return res;
+    }
+
+    void reverse(ListNode he,int c){
+        ListNode prev = null;
+        ListNode curr = he;
+        int size = c;
+
+        while(size-- > 0 && curr != null){
+            ListNode neww = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = neww;
+        }
+        
     }
 }
