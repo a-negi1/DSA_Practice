c7ee9c778cdb7431d1789916bb61a566d79d8e36
a-negi1 <adheeshnegi16@gmail.com>
2026-08-26 04:35:40 +0530

    LC 122 memoization

--- Java/Best Time to Buy and Sell Stock II.java
+++ Java/Best Time to Buy and Sell Stock II.java
@@ -35,3 +35,50 @@ class Solution {
 }
 }
 
+// memoization
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int k= 2;
+        int dp[][] = new int[n][k+1];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        int gain = solve (prices,n,0,k,dp);
+        
+        return gain;
+    }
+
+    int solve(int []prices,int n,int i,int target,int dp[][]){
+
+   
+    
+    if (i==n || target ==0){
+        return 0;
+    }
+
+    if( dp[i][target]!=-1){
+        return dp[i][target];
+    }
+    if (target == 2){
+        int c1=solve(prices,n,i+1,target-1,dp )-prices[i];
+        int c2= solve(prices,n,i+1,target,dp);
+
+        return dp[i][target]= Math.max(c1,c2);
+       
+    }
+    else{
+        
+
+       int c1=solve (prices,n,i+1,2,dp)+prices[i];
+       int c2 =solve(prices,n,i+1,target,dp);
+
+       return dp[i][target]= Math.max(c1,c2);
+    }
+    
+    
+}
+}
+
