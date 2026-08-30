09e92a1872a90df369f0b9ed3d6e143c3c24e39e
a-negi1 <adheeshnegi16@gmail.com>
2026-08-30 22:20:32 +0530

    LC 188 (bottom up approach)

--- Java/Best Time to Buy and Sell Stock IV.java
+++ Java/Best Time to Buy and Sell Stock IV.java
@@ -76,3 +76,40 @@ class Solution {
 
 }
 
+//tabulation
+
+class Solution {
+    public int maxProfit(int k, int[] prices) {
+
+        
+        int n= prices.length;
+        if(n==0){
+            return 0;
+        }
+        
+        int dp[][][] = new int[n+1][2*k+1][2];
+
+        for(int i=n-1;i>=0;i--){
+            for(int transaction = 1;transaction<=2*k;transaction++){
+
+                int buy = dp[i+1][transaction-1][0] - prices[i];
+                int notbuy = dp[i+1][transaction][1];
+
+                dp[i][transaction][1]= Math.max(buy,notbuy);
+
+                 int sell = dp[i+1][transaction-1][1] + prices[i];
+                 int notsell = dp[i+1][transaction][0];
+                 dp[i][transaction][0]= Math.max(sell,notsell);
+
+            }
+        }
+        
+        return dp[0][2*k][1];
+
+
+    }
+
+    
+
+
+}
