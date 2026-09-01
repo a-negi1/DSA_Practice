846f15330b24c36eb9e6eb54c51972bb3fa59816
a-negi1 <adheeshnegi16@gmail.com>
2026-09-01 18:42:03 +0530

    LC 1547 top down approach

--- Java/Minimum Cost to Cut a Stick.java
+++ Java/Minimum Cost to Cut a Stick.java
@@ -30,3 +30,48 @@ class Solution {
 
 }
 
+//memoization
+
+class Solution {
+    public int minCost(int n, int[] cuts) {
+        int len=cuts.length;
+        ArrayList <Integer> a  = new ArrayList<>();
+
+        
+        a.add(0);
+        for(int i=0;i<len;i++){
+            a.add(cuts[i]);
+        }
+        
+        a.add(n);
+        Collections.sort(a);
+
+        int[][] dp = new int[a.size()][a.size()];
+        
+        for(int i=0;i<a.size();i++ ){
+            Arrays.fill(dp[i],-1);
+        }  
+        return solve(a,1,a.size()-2,dp);
+    }
+
+    int solve(ArrayList <Integer>b,int i,int j,int[][] dp){
+
+        if(i>j){
+            return 0;
+        }
+
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int min = Integer.MAX_VALUE;
+        for(int k=i;k<=j;k++){
+            int curr = b.get(j+1)  - b.get(i-1) + solve(b,i,k-1,dp) + solve(b,k+1,j,dp) ;
+             min = Math.min(curr,min);
+            
+        }
+        return dp[i][j]= min;
+    }
+
+
+}
+
