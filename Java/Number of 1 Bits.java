6e50ef383bb414729e35856a90baff53e3a112fa
a-negi1 <adheeshnegi16@gmail.com>
2026-09-18 19:23:04 +0530

    LC 191(bitmasking)

---
+++ Java/Number of 1 Bits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int hammingWeight(int n) {
+
+        int count = 0;
+        for(int i=0;i<32;i++){
+            int bitmask = 1<<i;
+
+            if((n & bitmask) !=0){
+                count++;
+            }
+
+        }
+        return count;
+
+        
+    }
+}
